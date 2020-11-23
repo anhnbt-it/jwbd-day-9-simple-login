74a38117e30211d9096ab5d9e193be8e0c5e15d8
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2020-11-23 13:47:56 +0700

    first commit

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@javax.servlet.annotation.WebServlet(name = "LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        if (username.equals("admin") && password.equals("admin")) {
+            writer.println("<h1>Wellcome to " + username + "</h1>");
+        } else {
+            writer.println("Login error!");
+        }
+        writer.println("</html>");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
